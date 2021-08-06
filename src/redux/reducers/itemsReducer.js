9e2668fa50733015b27353073a68f80e70a23c6a
alejandrotoledoweb <atoledofr@gmail.com>
2021-08-05 20:55:55 -0500

    set itemsReducer to setItems and setError

---
+++ src/redux/reducers/itemsReducer.js
@@ -0,0 +1,33 @@
+import { ActionTypes } from '../constants/actionTypes';
+
+const initialState = {
+  items: [],
+  loading: true,
+  error: '',
+  status: '',
+};
+
+export const itemsReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_ALL_ITEMS:
+      return {
+        ...state,
+        loading: false,
+        devices: payload,
+        error: '',
+        status: 'set',
+        filter: 'All',
+      };
+    case ActionTypes.SET_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: payload,
+        status: 'error',
+      };
+    default:
+      return state;
+  }
+};
+
+export default itemsReducer;
