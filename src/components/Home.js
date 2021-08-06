d0e25a850bf63979e6eee0686f19d0a50864353e
alejandrotoledoweb <atoledofr@gmail.com>
2021-08-05 19:58:50 -0500

    create home component as home page and add bootstrap css and js inside html file

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../styles/home.css';
+
+function Home() {
+  return (
+    <main className="container container-sm container-md container-lg text-center">
+      <h2>Whale and Jaguar React Challenge</h2>
+      <p>Instructions:</p>
+      <p>
+        Go to the challenge page and see all the countries displayed on a list.
+      </p>
+    </main>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import Home from './components/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root'),
 );
