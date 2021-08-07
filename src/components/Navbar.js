952d2b93501bcef81f04321acb885b745c072522
alejandrotoledoweb <atoledofr@gmail.com>
2021-08-07 07:17:52 -0500

    fix name of components and add some style to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,51 +5,84 @@ import '../styles/navbar.css';
 
 const Navbar = () => (
   <header className="w-100 container d-flex border-top border-bottom pt-3 pb-3 rounded mt-3 flex-column">
-    <div className="d-flex justify-content-around">
-      <h4>
+    <div className="d-flex justify-content-around mb-3 flex-wrap">
+      <h4 className="title">
         <i className="fas fa-laptop-code" />
         <span>  </span>
         REACT CHALLENGE ATF
       </h4>
-      <Link to="/" className="btn btn-link">
+      <Link to="/" className="home-btn">
         Home
       </Link>
     </div>
-    <div className="d-flex justify-content-around flex-wrap border-top mt-3 pt-2">
-      <Link to="/items" className="btn btn-link btn-sm">
-        All Countries
-      </Link>
-      <Link to="/byname" className="btn btn-link btn-sm">
-        Search by Name
-      </Link>
-      <Link to="/byfullname" className="btn btn-link btn-sm">
-        Search by Full Name
-      </Link>
-      <Link to="/bycode" className="btn btn-link btn-sm">
-        Search by Code
-      </Link>
-      <Link to="/bycodelist" className="btn btn-link btn-sm">
-        Search by Code List
-      </Link>
-      <Link to="/bycurrency" className="btn btn-link btn-sm">
-        Search by Currency
-      </Link>
-      <Link to="/bylanguage" className="btn btn-link btn-sm">
-        Search by Language
-      </Link>
-      <Link to="/bycapital" className="btn btn-link btn-sm">
-        Search by Capital
-      </Link>
-      <Link to="/bycallingcode" className="btn btn-link btn-sm">
-        Search by Calling Code
-      </Link>
-      <Link to="/byregion" className="btn btn-link btn-sm">
-        Search by Region
-      </Link>
-      <Link to="/byregionalbloc" className="btn btn-link btn-sm">
-        Search by Regional Bloc
-      </Link>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+
+          <li className="nav-item">
+            <Link to="/items" className="links btn btn-link btn-sm">
+              All Countries
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/byfullname" className="links btn btn-link btn-sm">
+              Search by Full Name
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/bycode" className="links btn btn-link btn-sm">
+              Search by Code
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/bycodelist" className="links btn btn-link btn-sm">
+              Search by Code List
+            </Link>
+
+          </li>
+          <li className="nav-item">
+            <Link to="/bycurrency" className="links btn btn-link btn-sm">
+              Search by Currency
+            </Link>
+
+          </li>
+          <li className="nav-item">
+            <Link to="/bylanguage" className="links btn btn-link btn-sm">
+              Search by Language
+            </Link>
+
+          </li>
+          <li className="nav-item">
+            <Link to="/bycapital" className="links btn btn-link btn-sm">
+              Search by Capital
+            </Link>
+
+          </li>
+          <li className="nav-item">
+            <Link to="/bycallingcode" className="links btn btn-link btn-sm">
+              Search by Calling Code
+            </Link>
+
+          </li>
+          <li className="nav-item">
+            <Link to="/byregion" className="links btn btn-link btn-sm">
+              Search by Region
+            </Link>
+
+          </li>
+          <li className="nav-item">
+            <Link to="/byregionalbloc" className="links btn btn-link btn-sm">
+              Search by Regional Bloc
+            </Link>
+
+          </li>
+        </ul>
+      </div>
+    </nav>
 
   </header>
 );
--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Items from './components/Items';
 import Navbar from './components/Navbar';
-import byNameItems from './components/byName';
-import byFullName from './components/byFullName';
-import byCode from './components/byCode';
-import byCodeList from './components/byListOfCodes';
-import byCurrency from './components/byCurrency';
-import byLanguage from './components/byLanguage';
-import byCapital from './components/byCapital';
-import byCallingCode from './components/ByCallingCode';
-import byRegion from './components/byRegion';
-import byRegionalBloc from './components/byRegionalBloc';
+import ByNameItems from './components/ByName';
+import ByFullName from './components/ByFullName';
+import ByCode from './components/ByCode';
+import ByCodeList from './components/ByListOfCodes';
+import ByCurrency from './components/ByCurrency';
+import ByLanguage from './components/ByLanguage';
+import ByCapital from './components/byCapital';
+import ByCallingCode from './components/ByCallingCode';
+import ByRegion from './components/ByRegion';
+import ByRegionalBloc from './components/ByRegionalBloc';
 
 const Routes = () => (
   <BrowserRouter>
@@ -21,16 +21,16 @@ const Routes = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/items" component={Items} />
-        <Route path="/byname" component={byNameItems} />
-        <Route path="/byfullname" component={byFullName} />
-        <Route path="/bycode" component={byCode} />
-        <Route path="/bycodelist" component={byCodeList} />
-        <Route path="/bycurrency" component={byCurrency} />
-        <Route path="/bylanguage" component={byLanguage} />
-        <Route path="/bycapital" component={byCapital} />
-        <Route path="/bycallingcode" component={byCallingCode} />
-        <Route path="/byregion" component={byRegion} />
-        <Route path="/byregionalbloc" component={byRegionalBloc} />
+        <Route path="/byname" component={ByNameItems} />
+        <Route path="/byfullname" component={ByFullName} />
+        <Route path="/bycode" component={ByCode} />
+        <Route path="/bycodelist" component={ByCodeList} />
+        <Route path="/bycurrency" component={ByCurrency} />
+        <Route path="/bylanguage" component={ByLanguage} />
+        <Route path="/bycapital" component={ByCapital} />
+        <Route path="/bycallingcode" component={ByCallingCode} />
+        <Route path="/byregion" component={ByRegion} />
+        <Route path="/byregionalbloc" component={ByRegionalBloc} />
 
       </Switch>
     </main>
