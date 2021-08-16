5def1b01cc9ad36a2b638b025e24b3b16632d2dd
alejandrotoledoweb <atoledofr@gmail.com>
2021-08-16 10:28:41 -0500

    modify test for home component

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -5,9 +5,6 @@ test('renders learn home title', () => {
   render(<Home />);
   const linkElement = screen.getByText(/Whale and Jaguar React Challenge/i);
   expect(linkElement).toBeInTheDocument();
-
-  const linkElement2 = screen.getByText(/Go to each section and follow the instructions to fetch the data from each one./i);
-  expect(linkElement2).toBeInTheDocument();
 });
 
 test('renders learn home description', () => {
